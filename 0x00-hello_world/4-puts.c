2cf2006a4f9e5d51b885a108800ae809367c48f1
Mahmoud El-Turky <m.turkey614@gmail.com>
2023-07-13 18:40:03 -0700

    1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - to print on standard output
+ * using - the puts commands
+ * Return: Always Success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
