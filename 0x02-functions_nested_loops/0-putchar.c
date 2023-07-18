b27669ca4642f1dc5642dec517c50def2a33ffd8
Mahmoud El-Turky <m.turkey614@gmail.com>
2023-07-18 08:47:51 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,21 +3,23 @@
 
 /**
  * main - Entry point
- *
+ * @c: The character to be printed
  * Return: 0
  */
 
 int _putchar(char c)
 
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[] = _putchar;
+
+	_putchar(c[0]);
+	_putchar(c[1]);
+	_putchar(c[2]);
+	_putchar(c[3]);
+	_putchar(c[4]);
+	_putchar(c[5]);
+	_putchar(c[6]);
+	_putchar(c[7]);
 	_putchar("\n");
 
 	return (0);
