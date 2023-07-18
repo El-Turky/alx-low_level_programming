28a9a50e6d8b991c5ca99af4d53fcc7377c3e062
Mahmoud El-Turky <m.turkey614@gmail.com>
2023-07-18 09:33:43 -0700

    1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * @c: the lowercase character
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _islower(int c);
+{
+	if (c = 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
